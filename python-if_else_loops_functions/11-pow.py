3aac4f22247e3f9fb2aa96f2a9e5b0e775d64c01
Ausaryu <yonas.houriez@gmail.com>
2026-01-13 19:04:05 +0100

    fix 11

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    if b > 0:
-        for i in range(0, b):
-            result *= a
-    elif b == 0:
-        result = 1
-    else:
-        for i in range(0, abs(b)):
-            result = result / a
-    return result
+    return a ** b
