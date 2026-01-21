1d457ff2b00654480696fcfafacf8a4bdf5ce195
Ausaryu <yonas.houriez@gmail.com>
2026-01-21 16:06:17 +0100

    add documentation to 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+This module contains a function that prints a square using the character '#'.
+"""
+
+
 def print_square(size):
+    """
+    Prints a square of size `size` using the '#' character.
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if 0 > size:
