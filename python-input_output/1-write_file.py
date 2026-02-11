352485381057b411b3d646daeb886dde1290495b
Ausaryu <yonas.houriez@gmail.com>
2026-02-11 10:59:39 +0100

    fix some pycode style issues

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,7 +6,7 @@ This module contains a function that writes text to a file.
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file and returns the number of characters written.
+    Writes a string to a text file and returns the number of characters.
     """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
