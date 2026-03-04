f061fbe23064748151e846b7c691dd5f2b4bc083
Ausaryu <yonas.houriez@gmail.com>
2026-03-04 12:25:50 +0100

    fix 1 and 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 -- and display null if no genre linked.
 SELECT tv_genres.name AS 'genre', count(tv_show_genres.genre_id) AS 'number_of_shows'
 FROM tv_genres
-LEFT JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id
+JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
 ORDER BY count(tv_show_genres.genre_id) DESC;
