3042fa1080398470abeed9bde11db90e426ec2d6
Ausaryu <yonas.houriez@gmail.com>
2026-03-18 13:41:27 +0100

    refactor factorial function for improved readability and consistency

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 (function () {
   const a = parseInt(process.argv[2]);
-  function factorial(a) {
+  function factorial (a) {
     if (a > 1) {
       return a * factorial(a - 1);
-    }
-    else {
-      return 1
+    } else {
+      return 1;
     }
   }
   console.log(factorial(a));
