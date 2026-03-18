e3a146e6987f673e797b5122d053d8252e716dca
Ausaryu <yonas.houriez@gmail.com>
2026-03-18 13:41:04 +0100

    add script to calculate factorial of a number from command line arguments

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+(function () {
+  const a = parseInt(process.argv[2]);
+  function factorial(a) {
+    if (a > 1) {
+      return a * factorial(a - 1);
+    }
+    else {
+      return 1
+    }
+  }
+  console.log(factorial(a));
+})();
