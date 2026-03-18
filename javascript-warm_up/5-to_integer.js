257802bd29104b040655d4f48b1370ece9e0b7e1
Ausaryu <yonas.houriez@gmail.com>
2026-03-18 13:11:56 +0100

    add 5 t-o the project

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+(function () {
+  const num = Number(process.argv[2])
+  if (!isNaN(num)) {
+    console.log(`My number: ${num}`);
+  } else {
+    console.log('Not a number');
+  }
+})();
